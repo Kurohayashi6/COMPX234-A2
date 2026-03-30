a20e4ba61be213e7f479db46b9c3f4226448d8a4
Kurohayashi6 <2011169623@qq.com>
2026-03-30 20:28:53 +0800

    Start, join all threads and add final completion message.

--- readers_writers_starter.py
+++ readers_writers_starter.py
@@ -188,12 +188,17 @@ def main() -> None:
     all_threads = readers + writers
     
     # TODO: Start all threads
+    for thread in all_threads:
+        thread.start()
 
     
     # TODO: Wait for all threads to finish
+    for thread in all_threads:
+        thread.join()
 
 
     # TODO: Print final message that simulation completed
+    print("\nSimulation complete. All readers and writers have finished their tasks.")
 
 
 if __name__ == "__main__":
